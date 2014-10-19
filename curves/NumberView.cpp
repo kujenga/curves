e51838f883db8abb10bd7502388b3b590f66cd20
kujenga <ataylor0123@gmail.com>
2014-10-19 23:48:17 +0200

    correct positioning of bezier numbers

--- curves/NumberView.cpp
+++ curves/NumberView.cpp
@@ -17,9 +17,23 @@ void NumberView::setupCurve(int d)
 //    }
     
     curve1 = new Bezier();
+    curve2 = new Bezier();
+    curve3 = new Bezier();
+    curve4 = new Bezier();
     for (int i = 0; i < PT_COUNT ; i++) {
-        float2 pt = controlPoints[d][i]*0.005;
-        curve1->addControlPoint(transformedFloat2(pt));
+        float2 pt = (controlPoints[d][i]*float2(0.005, -0.005)) + float2(0.0, 1.0);
+        if (i <= 3) {
+            curve1->addControlPoint(transformedFloat2(pt));
+        }
+        if (i >= 3 && i <= 7) {
+            curve2->addControlPoint(transformedFloat2(pt));
+        }
+        if (i >= 6 && i <= 9) {
+            curve3->addControlPoint(transformedFloat2(pt));
+        }
+        if (i >= 9 && i <= 12) {
+            curve4->addControlPoint(transformedFloat2(pt));
+        }
     }
 }
 
@@ -29,4 +43,7 @@ void NumberView::draw()
         return;
     }
     curve1->draw();
+    curve2->draw();
+    curve3->draw();
+    curve4->draw();
 }
